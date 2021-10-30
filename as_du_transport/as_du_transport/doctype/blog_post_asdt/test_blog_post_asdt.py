fcd064af62797f1631185ef9e61423ec410cbc0b
yaacine <gy_zidelmla@esi.dz>
2021-10-30 21:44:50 +0100

    feat: blog post & horaires service doctypes

---
+++ as_du_transport/as_du_transport/doctype/blog_post_asdt/test_blog_post_asdt.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, yacine and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestBlogPostASDT(unittest.TestCase):
+	pass
--- as_du_transport/www/free_quote/index.py
+++ as_du_transport/www/free_quote/index.py
@@ -2,3 +2,4 @@ import frappe
 def get_context(context):
     context.users = ["yacine"]
     context.demandes_devis= frappe.get_list("Demande Devis", fields=['full_name', 'email'])
+    context.blogposts= frappe.get_list("Blog Post ASDT", fields=['title', 'read_time', 'published_on', 'published', 'blog_intro', 'blog_content', 'meta_image'])
