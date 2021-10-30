b46b16eae2cf27026767595df8114e31cfee9a7d
yaacine <gy_zidelmla@esi.dz>
2021-10-30 23:24:53 +0100

    fear: demande devis doctype

---
+++ as_du_transport/as_du_transport/doctype/demande_devis/demande_devis.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, yacine and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Demande Devis', {
+	// refresh: function(frm) {
+
+	// }
+});
