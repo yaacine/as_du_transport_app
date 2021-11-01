b379c76f15f6422bd3846985b71a2aff2facf09e
yaacine <gy_zidelmla@esi.dz>
2021-11-01 10:20:22 +0100

    feat: service demenagement doctype

---
+++ as_du_transport/as_du_transport/doctype/service_demenagement/service_demenagement.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, yacine and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Service demenagement', {
+	// refresh: function(frm) {
+
+	// }
+});
