05593a5de613ae1cf8dc436a019e7efce941993e
yaacine <gy_zidelmla@esi.dz>
2021-11-02 12:38:16 +0100

    feat: demenagement services index & details pages

--- as_du_transport/www/demenagement-services/details.py
+++ as_du_transport/www/demenagement-services/details.py
@@ -1,4 +1,5 @@
 import frappe
 def get_context(context):
-    context.single_blog= frappe.get_doc("Service demenagement", frappe.from_dict.docname )
+    context.single_service= frappe.get_doc("Service demenagement", frappe.form_dict.docname )
+    context.services= frappe.get_list("Service demenagement", fields=['name','service_title', 'short_description', 'full_description', 'service_full_image', 'service_short_image'] )
     
--- as_du_transport/www/our-blog/index.py
+++ as_du_transport/www/our-blog/index.py
@@ -1,3 +1,10 @@
 import frappe
 def get_context(context):
-    context.blogposts= frappe.get_list("Blog Post ASDT", fields=['title', 'read_time', 'published_on', 'published', 'blog_intro', 'blog_content', 'meta_image'])
+    context.blogposts= frappe.get_list(
+        "Blog Post ASDT", 
+        fields=['name','title', 'read_time', 'published_on', 'published', 'blog_intro', 'blog_content', 'meta_image'], 
+        filters={
+        'published': 1
+        }
+    )
+    
