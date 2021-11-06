cbda81182d0041f58d153bfec659421106076cd9
yaacine <gy_zidelmla@esi.dz>
2021-11-06 19:23:41 +0100

    feat: footer and other stuff

--- as_du_transport/templates/__init__.py
+++
@@ -1,6 +0,0 @@
-import frappe
-
-def get_context(context):
-    context.company_logo= frappe.get_all("Logo Entreprise", fields=['image'], start=0, page_length=1)
-    context.company_contacts= frappe.get_all("Contacts", fields=['mobile','email','telephone','adresse','facebook_account','twitter_account','linkedin_account'], start=0, page_length=1)
-    
