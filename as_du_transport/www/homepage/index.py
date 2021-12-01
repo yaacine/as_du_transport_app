b7dfa8e4b4d45cf99f1f19e573ba1a80747864f5
yaacine <gy_zidelmal@esi.dz>
2021-12-01 12:49:56 +0400

    feat: get services in homepage

--- as_du_transport/www/homepage/index.py
+++ as_du_transport/www/homepage/index.py
@@ -1,3 +1,4 @@
 import frappe
 def get_context(context):
     context.users = ["yacine"]
+    context.services= frappe.get_list("Service", fields=['name','service_title', 'short_description', 'full_description', 'service_full_image', 'service_short_image'] )
