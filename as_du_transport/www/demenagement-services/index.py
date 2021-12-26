35ab3c7fbb87297d034176867a648ad02e15fd74
yaacine <gy_zidelmal@esi.dz>
2021-12-26 22:45:58 +0400

    cleanup: comments

--- as_du_transport/www/demenagement-services/index.py
+++ as_du_transport/www/demenagement-services/index.py
@@ -1,7 +1,7 @@
 import frappe
 def get_context(context):
     context.services= frappe.get_list("Service demenagement", fields=['name','service_title', 'short_description', 'full_description', 'service_full_image', 'service_short_image'] )
-    print('#########################')
-    print(context.services)
-    print('#########################')
+    # print('#########################')
+    # print(context.services)
+    # print('#########################')
     
